938b2488a25e0e9720936c23036d9454df2afbf5
Joel7Anthony <anto777molina777@gmail.com>
2024-01-31 11:03:36 -0500

    categori

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -1,14 +1,14 @@
 const pool = require("../config/database.sql");
-const categories = require("..models/categorie.model");
+const  categories = require ("../models/categori.model")
 
 const Categories = {};
 
 Categories.getListCategories = async (req, res) => {
     const categories = await pool.query('SELECT * FROM  players');
-    res.render('Pages/categorie/list-categories', { categories });
+    res.render('Categories/categorie/list-categories', { categories });
   };
 
-  Categories.postCategories = async (req, res) => {
+  Categories.postCategori = async (req, res) => {
     const {
       under12, under15, under16, league, first, second, maxima, championofchampions
     } = req.body;
@@ -22,20 +22,20 @@ Categories.getListCategories = async (req, res) => {
 };
 
 //eliminar
-Categories.deleteCategories = async (req, res) => {
+Categories.deleteCategori = async (req, res) => {
     const { id } = req.params;
     await pool.query("DELETE FROM categories WHERE ID = ?", [id]);
     req.flash('success', 'Categoria Eliminado correctamente');
     res.redirect("/categories/list-categories");
   };
   //editar
-  Categories.getCategorie = async (req, res) => {
+  Categories.getCategori = async (req, res) => {
     const { id } = req.params;
     const categorie = await pool.query('SELECT * FROM categories WHERE id = ?', [id]);
-    res.render('Pages/categories/edit-categories', { categorie: categorie[0] });
+    res.render('Categories/categories/edit-categories', { categorie: categorie[0] });
   };
   //actualizar
-  Categories.updateCategories= async (req, res) => {
+  Categories.updateCategori= async (req, res) => {
     const { id } = req.params;
     const { under12, under15, under16, league, first, second, maxima, championofchampions
     } = req.body;
@@ -49,9 +49,9 @@ Categories.deleteCategories = async (req, res) => {
   };
 
   //agregar
-  Players.getAddCategories = async (req, res) => {
+  Categories.getAddCategories = async (req, res) => {
     const categories = await pool.query('SELECT * FROM  categories');
-    res.render('Pages/categories/categories', { categories });
+    res.render('Categories/categories/categories', { categories });
   };
   
   module.exports = Categories;
