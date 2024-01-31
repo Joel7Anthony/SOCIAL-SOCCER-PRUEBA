cf3365048815542e7f62eb326a3cfa4a83826582
yugsi022 <lcy.yugsi@yavirac.edu.ec>
2024-01-31 10:34:42 -0500

    controler-calendar

--- src/controllers/calendars.controller.js
+++ src/controllers/calendars.controller.js
@@ -1,38 +1,65 @@
-const pool = require("../config/database.sql");
-const calendars = require("../models/calendar.model");
+const pool = require("../config/dataBase.sql");
+const games = require("../models/game.model");
 
+const Games ={};
 
-const Calnedars = {};
+Games.getListGames = async (req, res) => {
+    const games = await pool.query('SELECT * FROM games');
+    res,render('Pages/game/list-games', {games});
 
-Calnedars.getListCalendars = async ( req, res) => {
-    const calendars = await pool.query('SELECT * FROM calendars');
-    res.render('Pages/calendar/list-calendars', {calendars});
 };
 
-Calnedars.getAddCalendars = async ( req, res) => {
-    res.render('Pages/calendar/calendars')
-};
-
-Calnedars.postCalendar = async( req, res) => {
+Games.postGame = async (req, res) => {
     const {
-        description, programguy, reminder, typeofprogram
+        namegame, descripcionGame,locationGame,imageGame
     } = req.body;
     const newLink = {
-        description, programguy,reminder,typeofprogram
+        namegame, descripcionGame,locationGame,imageGame
     };
-    await pool.query('INSERT INTO calendars set ?' [newLink]);
-    //Flash
-    req.flash('success', 'Agrefado Corectamente');
-    res.redirect("calendars/list-calendars");
+    await pool.query('INSERT INTO games set ?' [ newLink]);
+
+//FLASH
+
+req.flash('success', 'Partido agregado correctamente');
+res.redirect("/games/list-games");
+};
+
+//ELIMINAR 
+Games.deleteGame = async (req, res) => {
+    const { id } = req.params;
+        await pool.query('DELETE FROM games WHERE ID = ?', [id]);
+        req.flash('success', 'Partido Eliminado Correctamente');
+        res.redirect("/games/list-game");
+    };
+//EDITAR
+Games.getGame = async (req, res) => {
+    const { id } = req.params;
+        const game = await pool.query('SELECT * FROM games WHere id = ?', [id]);
+};
+
+
+//ACTUALIZAR
+Games.updateEgame = async (req, res) => {
+    const { id } = req.params;
+    const {namegame,descripcionGame,locationGame,imageGame
+    } = req.body;
+    const newLink = {
+        namegame,descripcionGame,locationGame,imageGame
+    };
+    console.log([id,newLink])
+    await pool.query('UPDATE games set ? WHERE id = ?', [newLink, id]);
+    req.flash('success', 'Partido editado correctamente');
+    res.redirect('/games/list-games'); 
 }
 
+//Agregar    
+Games.getAddGames = async (req, res) => {
+    const games = await pool.query('SELECT * FROM games');
+    res.render('Games/game/games', {games});
+};
+
+module.exports = Games;
 
 
 
 
-
-
-
-
-
-module.exports = Calnedars;
