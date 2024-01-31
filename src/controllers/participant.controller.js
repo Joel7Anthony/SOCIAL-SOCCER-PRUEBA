696a13ffd8d5e3fde9382aa260b555b58ace715a
Alexby32 <99358945+Alexby32@users.noreply.github.com>
2024-01-31 11:27:38 -0500

    cambios-participantes

--- src/controllers/participant.controller.js
+++ src/controllers/participant.controller.js
@@ -1,6 +1,3 @@
-const players = require("../models/player.model");
-const { param } = require("../routes/participants.routes");
-
 const pool = requiere ("../config/dataBase.sql");
 const participants = requiere ("../models/participant.model")
 
