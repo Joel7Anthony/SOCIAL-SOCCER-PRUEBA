c4fc47db9d7ca94b90169fb07467738b67cc56a2
Joel7Anthony <anto777molina777@gmail.com>
2024-02-09 19:29:22 -0500

    controller y model

--- src/controllers/photoPlayerstatistics.controller.js
+++ src/controllers/photoPlayerstatistics.controller.js
@@ -20,7 +20,7 @@ photoPlayerstatistics.updatePhoto = async (req, res) => {
 
     sampleFile.mv(uploadPath, function (err) {
         if (err) return res.status(500).send(err);
-        pool.query('UPDATE teamstatistics SET logo = ? WHERE id = ?', [sampleFile.name, id])
+        pool.query('UPDATE teamstatistics SET photo = ? WHERE id = ?', [sampleFile.name, id])
         req.flash('success', 'Foto actualizado');
         res.redirect('/playerstatistics');
 
